d96df626422754b6eb3075431d6a0e74730170c9
cfreitas <cfreitasrun@gmail.com>
2017-06-21 00:45:47 -0700

    aboutpagestyling

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -9,11 +9,11 @@
 
 get_header(); ?>
 
-<section class="home-page">
+<section class="about-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<div class='homepage-hero'>
-				<h2>Accelerate is a strategy and marketing agency <br>
+			<div class='about-hero'>
+				<h2><span>Accelerate</span> is a strategy and marketing agency <br>
 				located in the heart of NYC. Our goal is to build <br>
 				businesses by making our clients visible and <br>
 				making their customers smile.</h2>
@@ -22,29 +22,34 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
+
+<!-- services (beneath hero image) -->
+
 <section class="featured-work">
 	<div class="site-content">
-		<div class='homepage-hero'>
+		<div class='services-blurb'>
 			<h2><?php the_content(); ?></h2>
 
 
-		<ul class="homepage-featured-work">
+		<ul class="about-services">
 		<?php query_posts('posts_per_page=4&post_type=services'); ?>
 	  				<?php while ( have_posts() ) : the_post();
 										$image_1 = get_field("image_1");
 										$size = "medium";
 						?>
-						<li class="individual-featured-work">
+
+						<li class="individual-about-service">
+					    <figure class="about-image">
+					       <?php echo wp_get_attachment_image($image_1, $size); ?>
+					    </figure>
+					    <div class="about-description">
+					      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+					      <?php the_content(); ?>
+					    </div>
+
+					 </li>
 
 
-									<figure>
-												<?php echo wp_get_attachment_image($image_1, $size); ?>
-												<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-												<?php the_content(); ?>
-									</figure>
-
-
-						</li>
 	  				<?php endwhile; // end of loop. ?>
 	  				<?php wp_reset_query(); ?>
 		</ul>
@@ -54,7 +59,7 @@ get_header(); ?>
 
 			<div class="site-content">
 				<?php while ( have_posts() ) : the_post(); ?>
-					<div class='homepage-hero'>
+					<div class='contact-button'>
 						<h2>Interested in working at all?</h2>
 						<a class="button" href="<?php echo home_url(); ?>/blog">Contact Us</a>
 					</div>
